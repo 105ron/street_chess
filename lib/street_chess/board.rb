80ebcebf7c114e86e5cab94421bb7d04fa441b64
Austin P Mason <austinpmason@gmail.com>
2016-10-11 15:22:16 +0100

    write game_board specs

--- lib/street_chess/board.rb
+++ lib/street_chess/board.rb
@@ -1,4 +1,9 @@
 module StreetChess
   class Board
+    attr_accessor :game_board
+
+    def initialize
+      @game_board = Array.new(8) {Array.new(8)}
+    end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,7 +7,19 @@ module StreetChess
     it {expect(board).to be_instance_of Board}
 
     describe '#new' do
+      context 'when a new game_board is created' do
+        it 'should be an array' do
+          expect(board.game_board).to be_kind_of Array
+        end
 
+        it 'should have 8 rows' do
+          expect(board.game_board.length).to eql 8
+        end
+
+        it 'should have 8 columns in each row' do
+          expect(board.game_board[0].length).to eql 8
+        end
+      end
     end
   end
 end
